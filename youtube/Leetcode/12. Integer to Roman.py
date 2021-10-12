a31dc80bc5e3cd7946cc1c24f7e699511e29b322
abdallah <abdallah.m.daly@gmail.com>
2021-10-12 18:45:18 +0200

    Update 12. Integer to Roman.py

--- youtube/Leetcode/12. Integer to Roman.py
+++ youtube/Leetcode/12. Integer to Roman.py
@@ -1,14 +1,23 @@
 class Solution(object):
     def intToRoman(self, num):
         
-             
-        numeral_map = {1: "I", 4: "IV", 5: "V", 9: "IX", 10: "X", 40: "XL", 50: "L", 90: "XC", 100: "C", 400: "CD"}
-        keyset, result = sorted(numeral_map.keys()), ""
-        
-        while num > 0:
-            for key in reversed(keyset):
-                while num / key > 0:
-                    num -= key
-                    result += numeral_map[key]
-                    
-        return result
+        val = [
+            1000, 900, 500, 400,
+            100, 90, 50, 40,
+            10, 9, 5, 4,
+            1
+            ]
+        syb = [
+            "M", "CM", "D", "CD",
+            "C", "XC", "L", "XL",
+            "X", "IX", "V", "IV",
+            "I"
+            ]
+        roman_num = ''
+        i = 0
+        while  num > 0:
+            for _ in range(num // val[i]):
+                roman_num += syb[i]
+                num -= val[i]
+            i += 1
+        return roman_num
