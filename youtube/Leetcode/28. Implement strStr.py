221e4c220bf3df6975060617c72dd0f32fbe0802
abdallah <abdallah.m.daly@gmail.com>
2021-10-13 06:26:15 +0200

    Create 28. Implement strStr.py

---
+++ youtube/Leetcode/28. Implement strStr.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        i = 0
+        j = 0
+        m = len(needle)
+        n = len(haystack)
+        if m ==0:
+            return 0
+        while i<n and n-i+1>=m:
+            if haystack[i] == needle[j]:
+                temp = i
+                while j < m and i < n and needle[j] == haystack[i]:
+                    i += 1
+                    j += 1
+                if j == m:
+                    return temp
+                i= temp + 1
+                j = 0
+            else:
+                i += 1
+        return -1
