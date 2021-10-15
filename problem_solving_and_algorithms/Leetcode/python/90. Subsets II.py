a1608509b8588021bf203fb265c6849e345c2e5a
abdallah <abdallah.m.daly@gmail.com>
2021-10-15 13:50:29 +0200

    Create 90. Subsets II.py

---
+++ problem_solving_and_algorithms/Leetcode/python/90. Subsets II.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def subsetsWithDup(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        result = [[]]
+        previous_size = 0
+        
+        for i in xrange(len(nums)):
+            size = len(result)
+            
+            for j in xrange(size):
+                if i == 0 or nums[i] != nums[i - 1] or j >= previous_size:
+                    result.append(list(result[j]))
+                    result[-1].append(nums[i])
+            previous_size = size
+            
+        return result
