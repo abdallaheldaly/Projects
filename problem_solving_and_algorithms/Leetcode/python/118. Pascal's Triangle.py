5c7942ed60796e52a1e1b6eaaaf8cd82979511be
Abdallah El-Dely <abdallah.m.daly@gmail.com>
2021-10-27 01:21:16 +0200

    Create 118. Pascal's Triangle.py

---
+++ problem_solving_and_algorithms/Leetcode/python/118. Pascal's Triangle.py
@@ -0,0 +1,27 @@
+# [
+#      [1],
+#     [1,1],
+#    [1,2,1],
+#   [1,3,3,1],
+#  [1,4,6,4,1]
+# ]
+
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        
+        result = []
+        
+        for i in xrange(numRows):
+            result.append([])
+            for j in xrange(i + 1):
+                if j in (0, i):
+                    result[i].append(1)
+                else:
+                    result[i].append(result[i - 1][j - 1] + result[i - 1][j])
+        return result
+    
+    
