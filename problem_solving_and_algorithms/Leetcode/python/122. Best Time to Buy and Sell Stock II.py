13bbe40650fae7d90a6451cdcc455aa55696d6a1
Abdallah El-Dely <abdallah.m.daly@gmail.com>
2021-10-28 14:58:02 +0200

    Create 122. Best Time to Buy and Sell Stock II.py

---
+++ problem_solving_and_algorithms/Leetcode/python/122. Best Time to Buy and Sell Stock II.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        
+        ans = 0
+        
+        for i in range(1, len(prices)):
+            if prices[i] - prices[i - 1] > 0:
+                ans += (prices[i] - prices[i - 1])
+                
+        return ans
+    
+    
